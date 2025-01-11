d9945813005d938dd91adbe9493c821cbd28ad80
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2025-01-11 16:34:41 -0400

    gotta figure ot how to integrate the librarys

---
+++ Toolbox Class Library/ActiveSerials.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace RogersToolbox
+{
+    public class ActiveSerials
+    {
+        public List<SerialNumber> Serials { get; private set; }
+
+        public ActiveSerials(string serialString) 
+        {
+            Serials = new List<SerialNumber>(); 
+            string[] betweenList = serialString.Split("\n");
+
+            foreach (string rawSerial in betweenList)
+            {
+                Serials.Add(new SerialNumber(rawSerial.Trim())); 
+            }
+        }
+    }
+}
