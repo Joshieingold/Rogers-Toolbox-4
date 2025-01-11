47ea2af237690c9b4af7d1e4285d3341528a67fa
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2025-01-11 16:20:29 -0400

    got the old UI Added

--- RogersToolbox/ActiveSerials.cs
+++ RogersToolbox/ActiveSerials.cs
@@ -1,14 +1,20 @@
-﻿
-
+﻿using System.Collections.Generic;
 
 namespace RogersToolbox
 {
     public class ActiveSerials
     {
-        public List<SerialNumber> Serials;
-        public ActiveSerials(string SerialString) 
-        { 
-            SerialString.Trim();
+        public List<SerialNumber> Serials { get; private set; }
+
+        public ActiveSerials(string serialString) 
+        {
+            Serials = new List<SerialNumber>(); 
+            string[] betweenList = serialString.Split("\n");
+
+            foreach (string rawSerial in betweenList)
+            {
+                Serials.Add(new SerialNumber(rawSerial.Trim())); 
+            }
         }
     }
 }
