c1e598889e9c752f1398e4d9a5f0063261b17edf
Joshieingold <149664064+Joshieingold@users.noreply.github.com>
2025-01-21 23:50:50 -0400

    Added database class.

---
+++ Toolbox Class Library/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox_Class_Library
+{
+    public class Database
+    {
+        // This will be a class that holds all database connections.
+    }
+}
