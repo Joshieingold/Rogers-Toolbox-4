5987902e04753490cef59fb3212f1dd8369fd306
Josh <JoshLynch2001@gmail.com>
2025-03-09 14:47:08 -0300

    I think device goals is grabbed from firestre properly.

--- Toolbox Class Library/DatabaseConnection.cs
+++ Toolbox Class Library/DatabaseConnection.cs
@@ -36,12 +36,84 @@ namespace Toolbox_Class_Library
         {
 
         }
-
-        // Pulls device data based on month
-        public void PullDeviceData(string month)
+        private int DetermineMonth(string monthString)
         {
-
+            int month = 0;
+            switch (monthString)
+            {
+                case "January":
+                    month = 1;
+                    break;
+                case "February":
+                    month = 2;
+                    break;
+                case "March":
+                    month = 3;
+                    break;
+                case "April":
+                    month = 4;
+                    break;
+                case "May":
+                    month = 5;
+                    break;
+                case "June":
+                    month = 6;
+                    break;
+                case "July":
+                    month = 7;
+                    break;
+                case "August":
+                    month = 8;
+                    break;
+                case "September":
+                    month = 9;
+                    break;
+                case "October":
+                    month = 10;
+                    break;
+                case "November":
+                    month = 11;
+                    break;
+                case "December":
+                    month = 12;
+                    break;
+            }
+            return month;
         }
+        // Pulls device data based on month
+        public async Task<Dictionary<string, int>> PullGoalsData(string monthString)
+        {
+            int month = DetermineMonth(monthString);
+            DocumentReference goalsRef = _db.Collection("MonthlyGoals").Document(month.ToString());
+            DocumentSnapshot goalsSnapshot = await goalsRef.GetSnapshotAsync();
+
+            Dictionary<string, int> deviceGoals = new Dictionary<string, int>();
+
+            if (goalsSnapshot.Exists) // If it is found then update the dictionary with actual values.
+            {
+                Dictionary<string, object> goalsData = goalsSnapshot.ToDictionary();
+                deviceGoals["XB8Required"] = Convert.ToInt32(goalsData["CGM4981COM"]);
+                deviceGoals["XB7fcRequired"] = Convert.ToInt32(goalsData["CGM4331COM"]);
+                deviceGoals["XB7FCRequired"] = Convert.ToInt32(goalsData["TG4482A"]);
+                deviceGoals["Xi6tRequired"] = Convert.ToInt32(goalsData["IPTVTCXI6HD"]);
+                deviceGoals["Xi6ARequired"] = Convert.ToInt32(goalsData["IPTVARXI6HD"]);
+                deviceGoals["XiOneRequired"] = Convert.ToInt32(goalsData["SCXI11BEI"]);
+                deviceGoals["PodsRequired"] = Convert.ToInt32(goalsData["XE2SGROG1"]);
+            }
+            else // Default values for blue charts.
+            {
+                deviceGoals["XB8Required"] = 1;
+                deviceGoals["XB7fcRequired"] = 1;
+                deviceGoals["XB7FCRequired"] = 1;
+                deviceGoals["Xi6tRequired"] = 1;
+                deviceGoals["Xi6ARequired"] = 1;
+                deviceGoals["XiOneRequired"] = 1;
+                deviceGoals["PodsRequired"] = 1;
+            }
+
+            return deviceGoals;
+        }
+
 
         // Pushes device data to Firestore (to be implemented)
         public async Task PushDeviceData(string deviceName, int quantity, DateTime TimeOfTransaction, string user)
@@ -111,7 +183,24 @@ namespace Toolbox_Class_Library
             }
         }
     }
+    public static class DeviceGoals // Class that stores the data from the database.
+    {
+        public static int XB8Required { get; set; }
+        public static int XB7fcRequired { get; set; }
+        public static int XB7FCRequired { get; set; }
+        public static int Xi6tRequired { get; set; }
+        public static int Xi6ARequired { get; set; }
+        public static int XiOneRequired { get; set; }
+        public static int PodsRequired { get; set; }
 
+        public static int XB8Actual { get; set; } = 1;
+        public static int XB7fcActual { get; set; } = 1;
+        public static int XB7FCActual { get; set; } = 1;
+        public static int Xi6tActual { get; set; } = 1;
+        public static int Xi6AActual { get; set; } = 1;
+        public static int XiOneActual { get; set; } = 1;
+        public static int PodsActual { get; set; } = 1;
+    }
 
 
 
